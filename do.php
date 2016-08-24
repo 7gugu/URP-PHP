43aa350ede13a791bf52eefbc64852300bbe1d25
7gugu <763719129@qq.com>
2016-08-24 14:05:12 +0800

    up

--- do.php
+++ do.php
@@ -1,7 +1,7 @@
 <?php 
 require 'config/config.php';
 require 'function/dbcore.php';
-ignore_user_abort(true);
+//ignore_user_abort(true);
 set_time_limit(0);
 if(isset($_GET['cron'])){
 	header("Location:admin_panel.php?cron&suc=11");
@@ -89,6 +89,7 @@ exec("for /f \"tokens=1-5 delims= \" %a in ('\"netstat -ano|findstr \"^:{$port}\
  }
 query("update server set state='0'where port='{$rows['port']}'");
 }
+sleep(300);
 if($rocket['switch']==1){
 rocket_download($rocket['key']);
 getzip(PATHS."/Rocket.zip",PATHS."/unturned_data/Managed/");
