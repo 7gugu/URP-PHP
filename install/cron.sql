db06b2aa2fcb36bfdc5ba9dd0bc0a36e71b29d7d
7gugu <763719129@qq.com>
2016-09-16 22:05:09 +0800

    sql update

--- install/cron.sql
+++ install/cron.sql
@@ -1,7 +1,7 @@
 -- phpMyAdmin SQL Dump
 --
 -- 主机: localhost
--- 生成日期: 2016 年 09 月 16 日 22:00
+-- 生成日期: 2016 年 09 月 16 日 22:03
 -- 服务器版本: 5.5.40
 -- PHP 版本: 5.3.29
 
@@ -38,10 +38,12 @@ CREATE TABLE IF NOT EXISTS `cron` (
 INSERT INTO `cron` (`name`, `switch`, `key`, `text`) VALUES
 ('rocket', 0, '', ''),
 ('cron', 0, '', ''),
+('gamever', 0, '', ''),
 ('time', 0, '', ''),
 ('cmduser', 0, '', ''),
 ('cmdpaw', 0, '', ''),
-('cmdpath', 0, '', '');
+('cmdpath', 0, '', ''),
+('rocketver', 0, '', '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
