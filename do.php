d973fee95a0fe032cfd586635b5b5b0bdc43c103
7gugu <763719129@qq.com>
2016-10-04 15:36:04 +0800

    流程优化

--- do.php
+++ do.php
@@ -8,11 +8,18 @@ $rocket=mysqli_fetch_array(query("select * from cron where name='rocket'"));
 $time=mysqli_fetch_array(query("select * from cron where name='time'"));
 $cmd=mysqli_fetch_array(query("select * from cron where name='cmdpath'"));
 $api=json_decode(file_get_contents("http://api.rocketmod.net/status/unturned/".$rocket['key']),true);
+sleep(10);
 echo "http://api.rocketmod.net/status/unturned/".$rocket['key'];
+sleep(10);
 $server=false;
 $rocketver=false;
 $game=mysqli_fetch_array(query("select * from cron where name='gamever'"));
 $rocketver=mysqli_fetch_array(query("select * from cron where name='rocketver'"));
+/*
+if($api==""){
+     exit();
+}
+*/
 if($game['key']!=""&&$rocket['key']!=""){
     if($game['key']!="Unturned"){
     if($api[0]['gameversion']!=$game['key']){
@@ -36,6 +43,7 @@ echo "RocketVersion:".$r;
 query("update cron set `key`='{$g}'where `name`='gamever'");
 query("update cron set `key`='{$r}'where `name`='rocketver'");
 $update=mysqli_fetch_array(query("select * from cron where name='update'"));
+recurse_copy("D:\unturned\Servers","huifu");
 //var_dump($update);//sleep(60);
 //--------工作模块------------
 if($update['switch']==0){
@@ -65,6 +73,10 @@ while($rows = mysqli_fetch_array($rs)){
         $rows = mysqli_fetch_array (query("select * from server where port='{$rows['port']}'"));
         if($rows!=false){
 			echo $rows['sid'];
+			if($rows['sid']==""){
+				continue;
+			}
+			sleep(5);
        rcon($rows['sid'],0,1935,'');
       query("update server set `state`='1'where `port`='{$rows['port']}'");
         }
@@ -96,6 +108,31 @@ query("update cron set `switch`='0' where `name`='update'");
 }
 sleep(10);
 }else{
-	echo "Server update\n";
+	echo "Server update\n";sleep(10);
+	$rs=query("select * from server");
+while($rows = mysqli_fetch_array($rs)){
+        $rows = mysqli_fetch_array (query("select * from server where port='{$rows['port']}'"));
+		$dt=date('YMD',time());
+		if($time['key']!=$dt){
+			query("update cron set `key`='{$dt}'where `name`='time'");
+		if($time['switch']==1){
+    $date=$rows['time'];
+    $date=$date-1;
+	echo $date;
+    if($date<=-5){
+        $sid=$rows['sid'];
+       query("delete from server where sid='{$sid}'");
+    }elseif($date<=0){
+        rcon("save",1,$rows['rport'],$rows['rpw']);
+        $port=$rows['port']+1;
+		echo $port."\n";
+ system("for /f \"tokens=1-5 delims= \" %a in ('\"netstat -ano|findstr \"^:{$port}\"\"') do taskkill /f /pid %d ");
+    }
+query("update server set `time`='{$date}'where `port`='{$rows['port']}'");
+}
+		}else{
+						query("update cron set `key`='{$dt}'where `name`='time'");
+		}
+}
 }
 ?>
