59f954b0d750b193505797f7d68782b3cc630304
7gugu <763719129@qq.com>
2017-02-19 17:57:04 +0800

    debug

--- function/header.php
+++ function/header.php
@@ -5,10 +5,11 @@ echo "
     <strong>URP</strong> <small>后台管理</small>
   </div>
   <div class='am-collapse am-topbar-collapse' id='topbar-collapse'>
-
+<li class=‘am-dropdown’ data-am-dropdown>
     <ul class='am-nav am-nav-pills am-topbar-nav am-topbar-right admin-header-list'>
           <span class='am-icon-user'></span>   {$_SESSION['username']} 
      </ul>
+     </li>
   </div>
 </header>
 		 
