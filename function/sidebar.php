5f2d39767e8f8f5529564f64e6f78ecdd3b23090
7gugu <763719129@qq.com>
2018-02-20 17:42:29 +0800

    update sidebar.php

--- function/sidebar.php
+++ function/sidebar.php
@@ -37,17 +37,17 @@ echo "
  <div class='admin-sidebar am-offcanvas' id='admin-offcanvas'>
     <div class='am-offcanvas-bar admin-offcanvas-bar'>
       <ul class='am-list admin-sidebar-list'>
-        <li><a href='list.php'><span class='am-icon-home'></span>返回列表</a></li>
-        <li><a href='manage.php?index'><span class='am-icon-check'></span>实时状态</a></li>
-	    <li><a href='manage.php?information'><span class='am-icon-table'></span>信息设置</a></li>
-        <li><a href='manage.php?order'><span class='am-icon-terminal'></span>指令面板</a></li>
-		 <li><a href='manage.php?plugin'><span class='am-icon-plug'></span>插件管理</a></li>
-		  <li><a href='manage.php?map'><span class='am-icon-map-o'></span>上传地图</a></li>
-		  <li><a href='manage.php?mod'><span class='am-icon-wrench'></span>上传MOD</a></li>
-		  <li><a href='manage.php?players'><span class='am-icon-user'></span>在线玩家</a></li>
+        <li><a href='list.php'><span class='am-icon-home'></span> 返回列表</a></li>
+        <li><a href='manage.php?index'><span class='am-icon-check'></span> 实时状态</a></li>
+	    <li><a href='manage.php?information'><span class='am-icon-table'></span> 信息设置</a></li>
+        <li><a href='manage.php?order'><span class='am-icon-terminal'></span> 指令面板</a></li>
+		 <li><a href='manage.php?plugin'><span class='am-icon-plug'></span> 插件管理</a></li>
+		  <li><a href='manage.php?map'><span class='am-icon-map-o'></span> 上传地图</a></li>
+		  <li><a href='manage.php?mod'><span class='am-icon-wrench'></span> 上传MOD</a></li>
+		  <li><a href='manage.php?players'><span class='am-icon-user'></span> 在线玩家</a></li>
 		  ";
 	if(CSQL){	  
-		 echo "<li><a href='manage.php?msql'><span class='am-icon-skyatlas'></span>数据库管理</a></li>";
+		 echo "<li><a href='manage.php?msql'><span class='am-icon-skyatlas'></span> 数据库管理</a></li>";
 	}
 		  echo "
       </ul>  
@@ -73,13 +73,14 @@ echo "
  <div class='admin-sidebar am-offcanvas' id='admin-offcanvas'>
     <div class='am-offcanvas-bar admin-offcanvas-bar'>
       <ul class='am-list admin-sidebar-list'>
-	  <li><a href='list.php'><span class='am-icon-sign-out'></span>返回列表</a></li>
-        <li><a href='admin_panel.php'><span class='am-icon-home'></span>管理首页</a></li>
-        <li><a href='admin_panel.php?inser'><span class='am-icon-exchange'></span>管理激活码</a></li>
-	    <li><a href='admin_panel.php?muser'><span class='am-icon-users'></span>管理用户</a></li>
-		 <li><a href='admin_panel.php?cron'><span class='am-icon-tag'></span>计划任务</a></li>
-		  <li><a href='admin_panel.php?notice'><span class='am-icon-arrows'></span>发布公告</a></li>
-		   <li><a href='admin_panel.php?mplugin'><span class='am-icon-upload'></span>插件管理</a></li>
+	  <li><a href='list.php'><span class='am-icon-sign-out'></span> 返回列表</a></li>
+        <li><a href='admin_panel.php'><span class='am-icon-home'></span> 管理首页</a></li>
+        <li><a href='admin_panel.php?inser'><span class='am-icon-exchange'></span> 管理激活码</a></li>
+	    <li><a href='admin_panel.php?muser'><span class='am-icon-users'></span> 管理用户</a></li>
+		 <li><a href='admin_panel.php?cron'><span class='am-icon-tag'></span> 计划任务</a></li>
+		  <li><a href='admin_panel.php?notice'><span class='am-icon-arrows'></span> 发布公告</a></li>
+		   <li><a href='admin_panel.php?mplugin'><span class='am-icon-upload'></span> 插件管理</a></li>
+		   <li><a href='admin_panel.php?upgrade'><span class='am-icon-refresh'></span> 升级管理</a></li>
       </ul>  
 	   <div class='am-panel am-panel-default admin-sidebar-panel'>
         <div class='am-panel-bd'>
